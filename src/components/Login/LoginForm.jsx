b8e6e757549e6619ba9368fcd324b5668c051f2e
gebhartn <nicholas.gebhart@gmail.com>
2019-09-29 14:32:02 -0700

    Add Login skeleton
    Add skeleton with HOC withFormik to Login

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,3 +1,23 @@
 import React from "react";
 import { withFormik, Field, Form } from "formik";
-import { axiosWithAuth } from "../../utils/axiosWithAuth";
+import axiosWithAuth from "../../utils/axiosWithAuth";
+import * as Yup from "yup";
+
+const LoginForm = () => (
+  <div className="login">
+    <Form className="login__form">
+      <Field className="login__field"></Field>
+      <Field className="login__field"></Field>
+    </Form>
+  </div>
+);
+
+const FormikLogin = withFormik({
+  mapPropsToValues({}) {
+    return {
+      //
+    };
+  }
+})(FormikLogin);
+
+export default FormikLogin;
